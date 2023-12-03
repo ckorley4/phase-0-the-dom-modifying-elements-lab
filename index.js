a6ba872ae33d6285783faf154e809a305b4f1552
ckorley4 <ckorley4uo@yahoo.com>
2023-12-03 02:05:20 -0500

    Modifying DOM elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main = document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('H1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Nii Korley is the champion'
+document.body.append(newHeader)
